f2241bea9f674d50d56eaa703fa5c8f11639bb60
张阳浩 <zhangyanghao@zhangyanghaodeMacBook-Pro.local>
2018-05-02 21:17:08 +0800

    no message

---
+++ onePods/iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/InjectionBundle-Prefix.pch \
+  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/BundleContents.m \
+  /tmp/injectionforxcode/BundleInjection.h \
+  /tmp/injectionforxcode/BundleInterface.h \
+  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/BundleContents.h
