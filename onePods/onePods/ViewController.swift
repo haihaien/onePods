313e7213f5ed36cf5f3624bf4d8f686e9f8fd18a
张阳浩 <zhangyanghao@zhangyanghaodeMacBook-Pro.local>
2018-05-02 21:21:01 +0800

    no message

--- onePods/onePods/ViewController.swift
+++ onePods/onePods/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
