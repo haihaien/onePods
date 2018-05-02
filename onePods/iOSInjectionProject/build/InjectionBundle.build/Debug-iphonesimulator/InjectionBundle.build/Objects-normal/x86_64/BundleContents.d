0961797888a78ca49eaf77c640e78f70598280db
张阳浩 <zhangyanghao@zhangyanghaodeMacBook-Pro.local>
2018-05-02 21:18:53 +0800

    no message

--- onePods/iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/InjectionBundle-Prefix.pch \
-  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/zhangyanghao/Desktop/test/onePods/onePods/iOSInjectionProject/BundleContents.h
